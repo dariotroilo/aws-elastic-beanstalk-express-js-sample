cd3fd4c9684d7da4a3850366da6b46a795a23676
dariotroilo <dariotroilo@hotmail.com>
2021-03-08 12:33:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hola querido, bien hecho el cambio!'));
+app.get('/', (req, res) => res.send('Hola querido, full test!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
